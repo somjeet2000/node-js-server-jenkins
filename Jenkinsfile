c6921b86c17eb59fcc5235d2442e2fe3664554a3
Somjeet Srimani <somjeetsrimani2000@gmail.com>
2025-01-08 18:51:27 +0530

    Version 1

---
+++ Jenkinsfile
@@ -0,0 +1,9 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Clean Workspace') {
+            
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Node Server listening on port ${port}`);
+});
+
+// Test the API
+app.get('/', (req, res) => {
+  res.status(200).json({ message: '😊 API is running...' });
+});
+
+// Health check
+app.get('/health', (req, res) => {
+  res.status(200).json({ message: 'Everything is good here 🎉' });
+});
